d3975aea28c82cc46a0ef63326ff1fb78bcee58b
kakikubo <kakikubo@gmail.com>
2024-12-10 07:29:06 +0900

    他のブロックチェーンノードを探索する

---
+++ cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/kakikubo/blockchain-go/utils"
+)
+
+func main() {
+	fmt.Println(utils.FindNeighbors("127.0.0.1", 15000, 0, 3, 15000, 15003))
+	// Output: [127.0.0.1:15001 127.0.0.1:15002] 自身のポート番号が15000の場合、15001と15002が返される
+}
